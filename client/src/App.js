07431d55b917bde25cdf33cbc2b91606552065ee
kzh3010 <zcvpal2@naver.com>
2023-07-18 15:39:55 +0900

    feat: summary 페이지

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import SummaryPage from "./pages/SummayPage";
+
+function App() {
+  return (
+    <div className="App">
+      <SummaryPage />
+    </div>
+  );
+}
+
+export default App;
