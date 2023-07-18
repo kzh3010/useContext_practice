79d34a1a1d072bf171314e7a6d13fe3325e7442f
kzh3010 <zcvpal2@naver.com>
2023-07-18 16:44:25 +0900

    Feat: 백앤드에서 img 가져오기

--- client/src/components/Type.js
+++ client/src/components/Type.js
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Products from "./Products";
+import { Options } from "./Options";
 
 const Type = ({ orderType }) => {
+  //데이터 받아온거 기억
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    loadItems(orderType);
+  }, [orderType]);
+
+  const loadItems = async (orderType) => {
+    try {
+      const response = await axios.get(`http://localhost:4000/${orderType}`);
+      setItems(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const ItemComponent = orderType === "products" ? Products : Options;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
   return (
     <>
       <h2>{orderType}</h2>
@@ -12,7 +41,7 @@ const Type = ({ orderType }) => {
           flexDirection: orderType === "products" ? "row" : "column",
         }}
       >
-        item
+        {optionItems}
       </div>
     </>
   );
